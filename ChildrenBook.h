72282db3e03488d55633dcd7120fd7c6c73898fe
Joyce <joycerlz@uw.edu>
2023-02-21 22:42:15 -0800

    Added ifndef and include statements

--- ChildrenBook.h
+++ ChildrenBook.h
@@ -1,5 +1,10 @@
 // class for a children's book
 
+#ifndef CHILDRENBOOK_H
+#define CHILDRENBOOK_H
+
 class ChildrenBook {
     int copiesAvailable;   // 5 max
 };
+
+#endif
--- Patron.h
+++ Patron.h
@@ -1,5 +1,10 @@
 // class that holds patron data
 
+#ifndef PATRON_H
+#define PATRON_H
+
+#include <vector>
+
 class Patron {
     int idNumber;
     string firstName;
@@ -8,3 +13,4 @@ class Patron {
     vector<> booksCheckedOut;
 };
 
+#endif
