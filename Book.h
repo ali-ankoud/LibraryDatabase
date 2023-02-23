3ba67ccc26191f10058a4e06613721c0e06cf5a4
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2023-02-22 21:18:32 -0800

    Update Book.h
    Added constructors. Removed isCheckedOut, and added copiesAvailable to this class, to be inherited by child classes.

--- Book.h
+++ Book.h
@@ -7,12 +7,32 @@ using namespace std;
 
 class Book {
     public:
+    Book(){
+        // set default values
+    }
+    
+    Book(string name, string author, int day, int month, int year){
+        // set name, author, day, month, year
+        // do not set copiesAvailable: handled in child classes
+    }
+    
+    bool checkOut(){
+        // if(no copies left) return false
+        // decrement copiesAvailable
+        // return true
+    }
+    
+    bool checkIn(){
+        // if(max copies) return false
+        // increment copiesAvailable
+        // return true
+    }
 
     private:
         string name;
         string author;
         int day, month, year;
-        bool checkedOut;
+        int copiesAvailable;
 };
 
 #endif
