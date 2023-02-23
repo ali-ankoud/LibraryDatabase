84c3b61fa7dc09ddd9cd9c8ec81d9686bea29a76
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2023-02-22 21:02:02 -0800

    Update Patron.h
    Added template information for vectors
    Added pseudocode in greater detail for checkOut and returnItem: changed method headers slightly
    Added a constructor that takes in firstName, lastName, and idNumber

--- Patron.h
+++ Patron.h
@@ -9,11 +9,16 @@ class Patron {
     int idNumber;
     string firstName;
     string lastName;
-    vector<> checkOutHistory;
-    vector<> booksCheckedOut;
+    vector<Product> checkOutHistory;
+    vector<pair<Product, string>> booksCheckedOut;
 };
 
 void Patron::Patron(){
+    // generate ID number, first, last, and initialize vectors
+    // for checkout history and books checked out
+}
+
+void Patron::Patron(string firstName, string listName, int idNumber){
     // generate ID number, first, last, and initialize vectors
     // for checkout history and books checked out
 }
@@ -22,12 +27,22 @@ void Patron::~Patron(){
     // destructor for patron
 }
 
-void Patron::checkOut(/*product*/){
+bool Patron::checkOut(/*product*/){
     // code to check out book to patron
+    // if(product does not exist or has no copies) return false
+    // add product to booksCheckedOut
+    // add to checkOutHistory along with checked out status
+    // decrement product's available copies
+    // return true
 }
 
-void Patron::returnItem(/*product to return*/){
+bool Patron::returnItem(/*product to return*/){
     // code to return item to database
+    // if(product not in booksCheckedOut) return false
+    // remove product from booksCheckedOut
+    // add to checkOutHistory with checked in status
+    // increment product's available copies
+    // return true
 }
 
 #endif
