e33774af0bc9ae6447da87aa50a686ad5124620a
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2023-02-22 21:13:01 -0800

    Update nonfictionbook.h
    Added two constructors with pseudocode. Started file header with description of class.

--- nonfictionbook.h
+++ nonfictionbook.h
@@ -1,10 +1,24 @@
 // class for a non-fiction book
+// An object representing a non-fiction book. Has up
+// to five copies that can be checked out.
 
 #ifndef NONFICTIONBOOK_H
 #define NONFICTIONBOOK_H
 
 class NonFictionBook {
+    private:
     int copiesAvailable;   // 5 max
+    
+    public:
+    NonFictionBook(){
+        // sets copiesAvailable to 5
+        // calls parent no-arg constr
+    }
+    
+    NonFictionBook(string name, string author, int day, int month, int year){
+        // sets copiesAvailable to 5
+        // sets name, author, day, month, and year by calling parent constr
+    }
 };
 
 #endif
